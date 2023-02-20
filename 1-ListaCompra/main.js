56e56e3b388717ae7af6b5ae6f7fd93e8a348732
Carlos Soares <125346386+CarlosSoares123@users.noreply.github.com>
2023-02-20 15:40:40 +0100

    actualizar

---
+++ 1-ListaCompra/main.js
@@ -0,0 +1,28 @@
+
+  var miListaDeCompra = [
+  ['cereal', 3],
+  ['leite', 2],
+  ['salsicha', 6],
+  ['pao', 10],
+  ['agua', 3],
+  ['queijo', 5]
+]
+
+//cereal
+console.log("Vou comprar " + miListaDeCompra[0][1] + " unidades de " + miListaDeCompra[0][0] + "." )
+
+//leite
+console.log("Vou comprar " + miListaDeCompra[1][1] + " unidades de " + miListaDeCompra[1][0] + "." )
+
+//salsicha
+console.log("Vou comprar " + miListaDeCompra[2][1] + " unidades de " + miListaDeCompra[2][0] + "." )
+
+//pao
+console.log("Vou comprar " + miListaDeCompra[3][1] + " unidades de " + miListaDeCompra[3][0] + "." )
+
+//agua
+console.log("Vou comprar " + miListaDeCompra[4][1] + " unidades de " + miListaDeCompra[4][0] + "." )
+
+//queijo
+console.log("Vou comprar " + miListaDeCompra[5][1] + " unidades de " + miListaDeCompra[5][0] + "." )
+
