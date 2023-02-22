68e2167ad520b2f6a165d4bddb6f8ac6b4fdafc4
Carlos Soares <125346386+CarlosSoares123@users.noreply.github.com>
2023-02-22 14:29:32 +0100

    actualizar

---
+++ 4-RetornarValor/main.js
@@ -0,0 +1,21 @@
+function classificarValor(valor) {
+  var resposta;
+
+  switch (valor){
+    case 1:
+      resposta = "Alpha"
+      break;
+    case 2:
+      resposta = "beta"
+      break;
+    case 3:
+      resposta = "gamma"
+      break;
+    case 4:
+      resposta = "delta"
+      break;
+  }
+  return resposta
+}
+
+console.log(classificarValor(1))
